70c59672fae403ce99b064a09fce177bf0747f4c
Kevin Miller <kemiller2002@gmail.com>
2026-09-23 11:03:11 +0200

    Add scripts/install-toolchain.sh

---
+++ scripts/install-toolchain.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -eu
+
+INSTALL_BASE="${ECHELON_HOME:-$HOME/.echelon}"
+tmp="$(mktemp -d)"
+trap 'rm -rf "$tmp"' EXIT HUP INT TERM
+
+curl -fsSL https://raw.githubusercontent.com/kemiller2002/praxis/main/scripts/install-native.sh -o "$tmp/install-praxis.sh"
+sh "$tmp/install-praxis.sh" --install-base "$INSTALL_BASE"
+
+"$INSTALL_BASE/bin/echelon" install ordo
+
+printf '%s\n' "Echelon engineering toolchain installed."
+"$INSTALL_BASE/bin/echelon" doctor || true
